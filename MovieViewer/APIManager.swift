919923ec2cd2e32700a0eb5957a1d977aa52bf5e
Daniel Moreh <dmoreh@lyft.com>
2016-02-16 22:14:29 -0800

    Progress HUD and network error view

--- MovieViewer/APIManager.swift
+++ MovieViewer/APIManager.swift
@@ -12,7 +12,7 @@ class APIManager: NSObject {
 
     private static let kApiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
     
-    static func getMovies(completionHandler: ([Movie]?) -> Void) {
+    static func getMovies(completionHandler: ([Movie]?, NSError?) -> Void) {
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(kApiKey)")
         let request = NSURLRequest(URL: url!)
         let session = NSURLSession(
@@ -23,6 +23,10 @@ class APIManager: NSObject {
 
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
             completionHandler: { (dataOrNil, responseOrNil, errorOrNil) in
+                if let error = errorOrNil {
+                    completionHandler(nil, error)
+                    return
+                }
                 if let data = dataOrNil {
                     if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
                         data, options:[]) as? NSDictionary {
@@ -30,7 +34,7 @@ class APIManager: NSObject {
                             let moviesData = responseDictionary["results"] as! [NSDictionary]
                             let movies = moviesData.map { return Movie(data: $0) }
 
-                            completionHandler(movies)
+                            completionHandler(movies, nil)
                     }
                 }
         });
